03812e80e9d240af432c1076e889a80e325807e8
olaysco <olayiwolaodunsi@gmail.com>
2022-12-16 18:31:43 +0100

    Create Homepage container with background image

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -1,68 +1,106 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
+    <ion-content :fullscreen="true" class="ion-padding">
+      <div class="custom-header">
+        <div></div>
+      </div>
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <h1 class="app-name"><strong>LAPHA</strong></h1>
       </div>
     </ion-content>
+    <ion-footer class="footer ion-no-border ion-no-padding">
+      <ion-toolbar>
+        <ion-title class="ion-text-center ion-text-sm">
+          Lapha &copy; {{ year }}
+        </ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonPage,
+  IonFooter,
+  IonToolbar,
+  IonTitle,
+  isPlatform,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
-    IonContent,
-    IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonFooter,
+    IonContent,
+    IonToolbar,
+  },
+
+  setup() {
+    const year = new Date().getFullYear();
+    const isMobile = isPlatform("ios") || isPlatform("android");
+    return {
+      year,
+      isMobile,
+    };
+  },
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+ion-content {
+  --background: linear-gradient(162deg, rgb(0 0 0 / 80%) 20%, #177e89 100%),
+    url("../assets/images/home.svg") no-repeat 100% 100%;
+}
+
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
+
+  display: grid;
+  grid-row: 3;
+  grid-column: 1;
+  row-gap: 8vh;
+
+  padding: 0px 16px;
+
+  .app-name {
+    font-size: 20px;
+    line-height: 26px;
+    color: #fff;
+  }
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+#container .mode-btns {
+  grid-row: 2;
+  row-gap: 1vh;
+  display: grid;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-page,
+ion-app {
+  background-color: #177e89;
 }
 
-#container a {
-  text-decoration: none;
+ion-footer {
+  ion-toolbar {
+    --background: #177e89;
+  }
+  ion-title {
+    height: 100%;
+    font-size: small;
+  }
+}
+
+.custom-header {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
