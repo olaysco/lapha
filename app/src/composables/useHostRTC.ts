093bfb106c23521f5df07c33d7fcf995c0ec76b9
olaysco <olayiwolaodunsi@gmail.com>
2022-12-17 22:03:09 +0100

    Initiate and accept RTC connection while acting as signalling server

--- app/src/composables/useHostRTC.ts
+++ app/src/composables/useHostRTC.ts
@@ -1,7 +1,11 @@
+import { HTMLVideoElementWithCaptureStream } from "./../types";
+import { ref, onUnmounted } from "vue";
 declare let cordova: any;
 
 export function useHostRTC() {
   const wsserver = cordova.plugins.wsserver;
+  const peerConnections = ref<Map<string, RTCPeerConnection>>(new Map());
+  let peer: RTCPeerConnection;
 
   const startServer = () => {
     wsserver.start(
@@ -17,6 +21,7 @@ export function useHostRTC() {
         },
         onOpen: (conn: any) => {
           console.log(conn);
+          sendRTCOffer(conn);
         },
         onMessage: function (conn: any, msg: string) {
           handleMessage(conn, msg);
@@ -40,17 +45,86 @@ export function useHostRTC() {
     );
   };
 
+  const sendRTCOffer = async (conn: any) => {
+    peer = new RTCPeerConnection();
+    peer.onicecandidate = ({ candidate }) => {
+      console.log("icecandaidate");
+      sendMessage(conn.uuid, JSON.stringify({ candidate: candidate }));
+    };
+
+    const localVideo = <HTMLVideoElementWithCaptureStream>(
+      document.getElementById("video")
+    );
+    const stream = localVideo.captureStream();
+    stream.getTracks().forEach((track) => peer.addTrack(track, stream));
+
+    try {
+      const offer = await peer.createOffer();
+      await peer.setLocalDescription(offer);
+      sendMessage(conn.uuid, JSON.stringify({ offer: offer }));
+    } catch (error) {
+      console.log("error creating offer");
+      console.log(error);
+    }
+  };
+
   const handleMessage = async (conn: any, msg: string) => {
     console.log(`Received message ${msg} from ${conn.uuid}`);
     const msgObject = JSON.parse(msg);
+
+    if (msgObject.answer && peer) {
+      console.log(msgObject.answer);
+      await peer.setRemoteDescription(msgObject.answer);
+      console.log("aremote connected");
+      const dc = peer.createDataChannel("channel");
+      dc.onopen = () => {
+        console.log("dc opened");
+        dc.send("hello world");
+      };
+    }
   };
 
   const sendMessage = (to: string, message: string) => {
     if (wsserver == null) {
       return;
     }
+
     wsserver.send({ uuid: to }, message);
   };
+
+  const setLocalStream = () => {
+    const localVideo = <HTMLVideoElementWithCaptureStream>(
+      document.getElementById("video")
+    );
+    console.log("attempting to capture local stream");
+    console.log(
+      "attempting to capture local stream",
+      document.getElementById("stream-overlay")?.innerHTML
+    );
+
+    if (!localVideo) {
+      console.log("locals stream invalid");
+      return;
+    }
+
+    localVideo.onplay = () => {
+      console.log("locals stream captured");
+      console.log("count => " + peerConnections.value.size ?? 0);
+    };
+  };
+
+  const performSetup = () => {
+    setLocalStream();
+    startServer();
+  };
+
+  const performCleanup = () => {
+    console.log("unmounting");
+    wsserver.stop();
+    peerConnections.value = new Map();
+  };
+
+  onUnmounted(performCleanup);
 
   return {
     sendMessage,
