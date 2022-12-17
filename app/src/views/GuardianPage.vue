d01cff2817e2ca793b2996dc918474cf33fde0bb
olaysco <olayiwolaodunsi@gmail.com>
2022-12-17 21:34:28 +0100

    Display backdrop when user is attempting to create connection

--- app/src/views/GuardianPage.vue
+++ app/src/views/GuardianPage.vue
@@ -1,5 +1,12 @@
 <template>
   <ion-page>
+    <ion-backdrop :visible="joining"> </ion-backdrop>
+    <div class="backdrop-content" v-if="joining">
+      <div class="loader">
+        <ion-label>Establishing Connection</ion-label>
+        <ion-spinner name="crescent"></ion-spinner>
+      </div>
+    </div>
     <ion-content :fullscreen="true" class="ion-padding">
       <ion-grid>
         <ion-row>
@@ -11,7 +18,7 @@
         </ion-row>
       </ion-grid>
 
-      <div class="remote-overlay" id="remote-overlay">
+      <div class="remote-overlay" id="remote-overlay" v-show="hasJoin">
         <video id="remoteTrack" autoplay></video>
       </div>
     </ion-content>
@@ -19,16 +26,36 @@
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonPage, IonContent, IonCol, IonRow, IonGrid } from "@ionic/vue";
+import {
+  IonPage,
+  IonContent,
+  IonCol,
+  IonRow,
+  IonGrid,
+  IonBackdrop,
+  IonLabel,
+  IonSpinner,
+} from "@ionic/vue";
 import { useJoinRTC } from "../composables/useJoinRTC";
 import JoinPane from "../components/guardian/JoinPane.vue";
 
 export default defineComponent({
-  components: { IonPage, IonContent, IonCol, IonRow, IonGrid, JoinPane },
+  components: {
+    IonPage,
+    IonContent,
+    IonCol,
+    IonRow,
+    IonGrid,
+    JoinPane,
+    IonBackdrop,
+    IonLabel,
+    IonSpinner,
+  },
   setup() {
-    const { hasJoin, joinRTC } = useJoinRTC();
+    const { joining, hasJoin, joinRTC } = useJoinRTC();
     return {
       hasJoin,
+      joining,
       joinRTC,
     };
   },
@@ -49,6 +76,37 @@ export default defineComponent({
     width: 100vw;
     height: 100vh;
     object-fit: cover;
+  }
+}
+ion-backdrop {
+  opacity: 0.9;
+  &.backdrop-hide {
+    display: none;
+  }
+  &.backdrop-show {
+    display: block;
+  }
+}
+.backdrop-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  height: 100px;
+  border-radius: 10px;
+
+  & > .loader {
+    display: flex;
+    align-items: center;
+
+    & > ion-label {
+      margin-right: 0.4em;
+    }
   }
 }
 </style>
